1781863d63bfb8a8ef5ca3414ddc0cad2cc038f8
karan <karanverma201411@gmail.com>
2023-12-30 14:39:32 +0530

    first

---
+++ problem1.js
@@ -0,0 +1,22 @@
+// Problem-1) print even numbers from 0 to 10
+
+// so we have even numbers 2,4,6,8 and 10 from (0-10) and we have to print 2,4,6,8,10 only
+
+// we can find it by finding the modulus of each number, if zero then it's an even number, if not zero, then it's not an even number 
+
+for(let i = 0; i<=10; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    else{
+        console.log(`${i} is not even number`);
+    }
+}
+
+// let i= 0;
+// while(i<=10){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     i++;
+// }
