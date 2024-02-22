53437bc544a5661adacf909deb7812e21991c31a
karan <karanverma201411@gmail.com>
2024-02-22 18:23:48 +0530

    hoisting in js

---
+++ Advanced_JavaScript/hoisting.js
@@ -0,0 +1,15 @@
+// console.log(a);
+// var a = 5;
+
+// console.log(a);
+// let a= 5;
+
+// greet();
+
+// function greet(){
+//     console.log('welome to my nodejs environment :)');
+// }
+
+/*const greet = () =>{
+    console.log('welcome to my nodejs environment :)');
+ }  arrow functions aren't hoisted */
