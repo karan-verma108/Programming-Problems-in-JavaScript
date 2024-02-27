3aa4dc7115ba7d2f7ebf7e8ec8e196a453147f5d
karan <karanverma201411@gmail.com>
2024-02-27 22:10:24 +0530

    tested strict mode

--- Advanced_JavaScript/Lexical/index.js
+++ Advanced_JavaScript/Lexical/index.js
@@ -1,16 +1,18 @@
+// 'use strict';
+
 let a = 'hi guys!';
 
-function funA () {
+function funA() {
     let b = '... how are you?';
     // console.log(c);  //but the parent function can't have access to its child's variables
     funB();
 
-    function funB () {
+    function funB() {
         let c = '... I hope you are good!';
         console.log(a + b + c);  //this will have access to its parent variables due to lexical scoping, only in case of traditional function
         funC();
 
-        function funC(){
+        function funC() {
             console.log(`trying to access b and c : ${b} and ${c}`); //this time it will work bcoz funC() now is a child of funB() which is a child of funA()
         }
 
@@ -21,5 +23,16 @@ function funA () {
 //     console.log(`Im trying to access b and c : ${b} ${c}`); //wont work as the scope of b and c is only limited to funA() and funB() and is not accessible by funC() because it's a child function of funA() nor of funB()
 // }
 
-funA();
+// funA();
 // funC();
+
+// testing use strict mode 
+
+
+const sum = (a, b) => {
+    // console.log(add);
+    let add = a + b;
+    console.log(add);
+}
+
+sum(10, 3);
