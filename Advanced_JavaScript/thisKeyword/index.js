cdfc1a92a577bff42dd1b33ae419f1cad08c415f
karan <karanverma201411@gmail.com>
2024-02-28 23:45:41 +0530

    this keyword

---
+++ Advanced_JavaScript/thisKeyword/index.js
@@ -0,0 +1,22 @@
+'use strict';
+// console.log(this);  //refers to the global object
+
+// function add (a,b) {
+//     let sum = a+b;
+//     console.log(sum);
+//     console.log(this); //it also refers to the global object, when strict mode is used, this becomes undefined in regular expressions
+// }
+
+// add(3,4); 
+
+const data = {
+    name : 'karan',
+    degree : 'btech',
+    sum : function(a,b){
+        let add = a+b;
+        console.log(add);
+        console.log(this);  //this here refers to this data object
+    }
+}
+
+data.sum(3,4);
